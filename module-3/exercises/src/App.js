41c6b55a34a6855c0c6051d1952f897b1b828cc0
Christine <christinetrant@gmail.com>
2025-06-26 13:21:28 +0100

    Add module 3 exercises

---
+++ module-3/exercises/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import BreadcrumbsExercise from "./Breadcrumbs";
+import ButtonTable from "./ButtonTable";
+
+function App() {
+	return (
+		<div className="App">
+			<BreadcrumbsExercise />
+			<ButtonTable />
+		</div>
+	);
+}
+
+export default App;
