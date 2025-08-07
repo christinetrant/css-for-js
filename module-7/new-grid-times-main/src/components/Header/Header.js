f266be4bf5c53992aa593210e50f47f4151a1409
Christine <christinetrant@gmail.com>
2025-08-07 16:40:43 +0100

    Add Desktop header to module 7 workoshop

--- module-7/new-grid-times-main/src/components/Header/Header.js
+++ module-7/new-grid-times-main/src/components/Header/Header.js
@@ -1,70 +1,116 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
-  return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
-  );
+	return (
+		<header>
+			<SuperHeader>
+				<Row>
+					<ActionGroup>
+						<button>
+							<Search size={24} />
+						</button>
+						<button>
+							<Menu size={24} />
+						</button>
+					</ActionGroup>
+					<ActionGroupMobile>
+						<button>
+							<User size={24} />
+						</button>
+					</ActionGroupMobile>
+
+					<DesktopItem>
+						<Logo />
+					</DesktopItem>
+					<DesktopItem>
+						<SubscribeActions>
+							<Button>
+								<a href="">Subscribe</a>
+							</Button>
+							<a href="">Already a subscriber?</a>
+						</SubscribeActions>
+					</DesktopItem>
+				</Row>
+			</SuperHeader>
+			<MainHeader>
+				<Logo />
+			</MainHeader>
+		</header>
+	);
 };
 
 const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
+	padding: 16px 0;
+	background: var(--color-gray-900);
+	color: white;
+	@media ${QUERIES.laptopAndUp} {
+		background: white;
+		color: var(--color-gray-900);
+	}
 `;
 
 const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const ActionGroup = styled.div`
-  display: flex;
-  gap: 24px;
+	display: flex;
+	gap: 24px;
 
-  /*
+	/*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
   */
-  svg {
-    display: block;
-  }
+	svg {
+		display: block;
+	}
+`;
+
+const DesktopItem = styled.div`
+	display: none;
+	@media ${QUERIES.laptopAndUp} {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+	}
+`;
+
+const SubscribeActions = styled.div`
+	display: none;
+	@media ${QUERIES.laptopAndUp} {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		gap: 8px;
+	}
+`;
+
+const ActionGroupMobile = styled(ActionGroup)`
+	@media ${QUERIES.laptopAndUp} {
+		display: none;
+	}
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-top: 32px;
+	margin-bottom: 48px;
+	@media ${QUERIES.laptopAndUp} {
+		display: none;
+	}
 `;
 
 export default Header;
